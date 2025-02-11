a8b9c4c39667540f01df16fed43be5e041d52d6d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-02-11 03:36:12 -0500

    Update run-outside-docker.sh

--- run-outside-docker.sh
+++ run-outside-docker.sh
@@ -1,28 +1,38 @@
 #/bin/sh
-# Prepares binaries and environment and runs op-geth based on 
+# Prepares binaries and environment to run op-geth based on 
 # section op-geth: of docker-composer.yml and variables of alfajores.env
 
 # image: us-west1-docker.pkg.dev/devopsre/celo-blockchain-public/op-geth:celo-v2.0.0-rc3
+cd ..
 git clone https://github.com/celo-org/op-geth.git
 cd  op-geth
+git checkout celo-v2.0.0-rc3
 make
-
-# volumes:
-# - ./envs/${NETWORK_NAME}/config:/chainconfig
-mkdir /chainconfig
-cp -rf envs/alfajores/config/* /chainconfing
-# - ./scripts/:/scripts
-mkdir /scripts
-cp -rf scripts/* /scripts/
-# - shared:/shared
-mkdir /shared
-# - ${DATADIR_PATH}:/geth alfajores.env includes DATADIR_PATH=./envs/${NETWORK_NAME}/datadir
-mkdir /geth
+export PATH="$PATH:`pwd`/build/bin"
+cd ../celo-l2-node-docker-compose/
 
 # env_file:
 # - ./envs/${NETWORK_NAME}/op-geth.env
 cp envs/alfajores/op-geth.env .env
 # - .env
 
+# volumes:
+# - ./envs/${NETWORK_NAME}/config:/chainconfig
+sudo mkdir /chainconfig
+sudo chown $(whoami):$(whoami) /chainconfig
+cp -rf envs/alfajores/config/* /chainconfig/
+# - ./scripts/:/scripts
+sudo mkdir /scripts
+sudo chown $(whoami):$(whoami) /scripts
+cp -rf scripts/* /scripts/
+# - shared:/shared
+sudo mkdir /shared
+sudo chown $(whoami):$(whoami) /shared
+# - ${DATADIR_PATH}:/geth alfajores.env includes DATADIR_PATH=./envs/${NETWORK_NAME}/datadir
+sudo mkdir /geth
+sudo chown $(whoami):$(whoami) /geth
+
+# entrypoint: /scripts/start-op-geth.sh
+/scripts/start-op-geth.sh
 
 
